b51f11d7b11ca424e28db3a3be187ef574a173eb
Brett Fodor <bmattmusic@gmail.com>
2019-09-09 15:32:46 -0400

    Game Menu Start and Exit

--- VirtualPetsAmok/Program.cs
+++ VirtualPetsAmok/Program.cs
@@ -6,7 +6,45 @@ namespace VirtualPetsAmok
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // variables
+            string userInput;
+
+            bool inGame = false;
+            while (1 == 1)
+            {
+                //Game menu
+                while (inGame == false)
+                {
+
+                    Console.WriteLine("Virtual Pets Amok");
+                    Console.WriteLine("Game Menu");
+
+                    Console.WriteLine("Play Game (p)");
+
+                    Console.WriteLine("Exit Game (e)");
+
+
+                    userInput = Console.ReadLine();
+                    if (userInput.ToLower() == "p")
+                    {
+                        inGame = true;
+                    }
+
+                }
+                //Playing Game
+                while (inGame)
+                {
+
+                    Console.WriteLine("To Return to Main Menu: type (m)");
+
+                    userInput = Console.ReadLine();
+                    if (userInput.ToLower() == "m")
+                    {
+                        inGame = false;
+                    }
+
+                }
+            }
         }
     }
 }
--- VirtualPetsAmok/VirtualPets.cs
+++ VirtualPetsAmok/VirtualPets.cs
@@ -6,5 +6,6 @@ namespace VirtualPetsAmok
 {
     class VirtualPets
     {
+
     }
 }
