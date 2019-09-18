79a27d7cfe66d622826f4d6a9e1b0039c83c81ea
Brett Fodor <bmattmusic@gmail.com>
2019-09-18 10:39:49 -0400

    Added Select Pet Method and Interact with Selected pet menu

--- VirtualPetsAmok/Menu.cs
+++ VirtualPetsAmok/Menu.cs
@@ -47,6 +47,7 @@ namespace VirtualPetsAmok
                 //menu
                 Console.WriteLine("To Return to Main Menu: type (m)");
                 Console.WriteLine("To create a pet: type (c)");
+                Console.WriteLine("To select an individual pet; type (!)");
 
                 Console.WriteLine("\nInteract with pet");
                 Console.WriteLine("To check your pets information: type (i)");
@@ -54,6 +55,7 @@ namespace VirtualPetsAmok
                 Console.WriteLine("To feed your pet: type (f)");
                 Console.WriteLine("To play with your pet: type (p)");
                 Console.WriteLine("To take your pet to the doctor: type (d)");
+                
                 userInput = Console.ReadLine();
                 if (userInput.ToLower() == "m")
                 {
@@ -83,10 +85,97 @@ namespace VirtualPetsAmok
                 {
                     myShelter.AllDoctorPet();
                 }
+                else if (userInput.ToLower() == "!")
+                {
+                    
+                   VirtualPets selectedPet = this.SelectPet(myShelter);
+                    this.InteractWithSelectedPet(selectedPet);
+                }
                 Console.Clear();
             }
+
+            
+        }
+        public VirtualPets SelectPet(VirtualPetShelter myShelter)
+        {
+            int i = 1;
+
+            Console.WriteLine("Please choose the number of the pet you'd like to interact with");
+            foreach (VirtualPets pets in myShelter.petsInShelter)
+
+            {
+                Console.WriteLine($"{i}. {pets.Name}");
+                i++;
+            }
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            VirtualPets selectedPet = myShelter.petsInShelter[userInput - 1];
+
+            return selectedPet;
+        }
+        public void InteractWithSelectedPet(VirtualPets selectedPet)
+        {if (selectedPet is RoboticPet)
+            {
+                Console.WriteLine("\nInteract with pet");
+                Console.WriteLine("To check your pets information: type (i)");
+                Console.WriteLine("To check your pets status: type (s)");
+                Console.WriteLine("To charge your pet's battery: type (f)");
+                Console.WriteLine("To play with your pet: type (p)");
+                Console.WriteLine("To take your pet to the mechanic: type (d)");
+            }
+         else
+            {
+                Console.WriteLine("\nInteract with pet");
+                Console.WriteLine("To check your pets information: type (i)");
+                Console.WriteLine("To check your pets status: type (s)");
+                Console.WriteLine("To feed your pet: type (f)");
+                Console.WriteLine("To play with your pet: type (p)");
+                Console.WriteLine("To take your pet to the doctor: type (d)");
+
+            }
+
+            string userInput = Console.ReadLine();
+            
+            
+           
+            if (userInput.ToLower() == "i")
+            {
+                Console.Clear();
+                selectedPet.CheckInfo();
+                Console.WriteLine("\nPress 'Enter' to continue");
+                Console.ReadLine();
+
+            }
+            else if (userInput.ToLower() == "s")
+            {
+                Console.Clear();
+                selectedPet.CheckStatus();
+                Console.WriteLine("\nPress 'Enter' to continue");
+                Console.ReadLine();
+            }
+            else if (userInput.ToLower() == "f")
+            {
+                Console.Clear();
+                selectedPet.FeedPet();
+                Console.WriteLine("\nPress 'Enter' to continue");
+                Console.ReadLine();
+            }
+            else if (userInput.ToLower() == "p")
+            {
+                Console.Clear();
+                selectedPet.PlayPet();
+                Console.WriteLine("\nPress 'Enter' to continue");
+                Console.ReadLine();
+            }
+            else if (userInput.ToLower() == "d")
+            {
+                Console.Clear();
+                selectedPet.DoctorPet();
+                Console.WriteLine("\nPress 'Enter' to continue");
+                Console.ReadLine();
+            }
+            Console.Clear();
         }
 
-       
     }
 }
